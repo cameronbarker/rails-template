1a28bcf890a218002b4469791862122d7e7ad004
Cameron Barker <barker.cameron@gmail.com>
2021-02-23 21:26:51 -0700

    adding urls to snippets

--- _kitchen_sink.rb
+++ _kitchen_sink.rb
@@ -3,28 +3,28 @@ git add: "."
 git commit: "-m 'Initial commit'"
 
 # Template: RSPEC, FactoryBot, Standard
-run "rails app:template LOCATION='scripts/testing'"
+run "rails app:template LOCATION='https://raw.githubusercontent.com/cameronbarker/ruby-snippets/main/scripts/testing.rb'"
 
 # Set up Configs
-run "rails app:template LOCATION='scripts/configs'"
+run "rails app:template LOCATION='https://raw.githubusercontent.com/cameronbarker/ruby-snippets/main/scripts/configs.rb'"
 
 # Install: Devise
-run "rails app:template LOCATION='scripts/authentication'"
+run "rails app:template LOCATION='https://raw.githubusercontent.com/cameronbarker/ruby-snippets/main/scripts/authentication.rb'"
 
 # Install: Errors
-run "rails app:template LOCATION='scripts/errors'"
+run "rails app:template LOCATION='https://raw.githubusercontent.com/cameronbarker/ruby-snippets/main/scripts/errors.rb'"
 
 # Set Up Heroku
-run "rails app:template LOCATION='scripts/'heroku"
+run "rails app:template LOCATION='https://raw.githubusercontent.com/cameronbarker/ruby-snippets/main/scripts/heroku.rb'"
 
 # Marketing Pages and SEO"
-run "rails app:template LOCATION='scripts/marketing'"
+run "rails app:template LOCATION='https://raw.githubusercontent.com/cameronbarker/ruby-snippets/main/scripts/marketing.rb'"
 
 # Set up staging site
-run "rails app:template LOCATION='scripts/staging'"
+run "rails app:template LOCATION='https://raw.githubusercontent.com/cameronbarker/ruby-snippets/main/scripts/staging.rb'"
 
 # Template: HAML
-run "rails app:template LOCATION='scripts/haml'"
+run "rails app:template LOCATION='https://raw.githubusercontent.com/cameronbarker/ruby-snippets/main/scripts/haml.rb'"
 
 puts "Set Up DB"
 rails_command("db:create")
