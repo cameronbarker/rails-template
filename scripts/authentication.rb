f20f6412b5a7360d2ff4dca5e739e02ca1fa1de6
Cameron Barker <barker.cameron@gmail.com>
2021-09-28 14:45:27 -0700

    Bcrypt be better

--- scripts/authentication.rb
+++ scripts/authentication.rb
@@ -1,6 +1,7 @@
 puts "Adding: Devise as User"
+run "bundle add bcrypt"
 
-run "bundle add bcrypt devise"
+run "bundle add devise"
 run "rails generate devise:install"
 run "rails generate devise:views"
 
